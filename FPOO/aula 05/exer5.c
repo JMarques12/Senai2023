ea848145b7f0c695420d7fb8d50a7092c6ee2d64
JMarques12 <joaovcmarques12@gmail.com>
2023-08-10 10:41:13 -0300

    att

---
+++ FPOO/aula 05/exer5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int x;
+	
+	printf("digite um numero: ");
+	scanf("%d",&x);
+	while(x!=4){
+		printf("digite um numero: ");
+	scanf("%d",&x);
+	}
+	
+	
+	return 0;
+}
