6e469a2c89a273459bfa7d0c4f696ecb4a4bb28a
JMarques12 <gustavo.santos.contato@hotmail.com>
2023-09-13 11:30:37 -0300

    att

---
+++ FPOO/aula 11/2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+int media(int nota1, int nota2,int nota3){
+	return ( nota1 + nota2 + nota3) / 3;
+}
+
+//Exemplo de um procedimento que escreve uma frase 100 vezes
+int procedimento(){
+	int i;
+	for( i = 0; i < 6; i++)
+		printf("reprovado");
+}
+
