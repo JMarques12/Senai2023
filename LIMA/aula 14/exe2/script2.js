ed3c7000dff671ee94b39001c7703cedb29379f0
IgorHenriqueG <IgorHGoncalvess@gmail.com>
2023-11-30 11:29:56 -0300

    att

---
+++ LIMA/aula 14/exe2/script2.js
@@ -0,0 +1,14 @@
+function contar() {
+    let saida = document.getElementById('saida')
+
+    saida.innerHTML += `<h2>Contando de 1 até 10, marcando os pares</h2>`
+
+    let cont = 1
+    while (cont <= 10) {
+        if (cont % 2 == 0) {
+            saida.innerHTML += `<mark><strong> ${cont} </strong></mark>,`    
+        } else {
+            saida.innerHTML += ` ${cont} ,`
+        }
+        cont ++
+    }}
